9593cfc2a5929780783adccebe6b19675e45006d
194156-QKHAI\quang <lqkhai.dev@gmail.com>
2022-12-05 09:42:33 +0700

    upd Lesson3

--- src/test/java/PlsDontBug/debug.java
+++ src/test/java/PlsDontBug/debug.java
@@ -1,28 +1,17 @@
 package PlsDontBug;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import org.testng.annotations.Test;
 
 
 public class debug {
+    private static final Logger log = LogManager.getLogger(debug.class.getName());
     @Test
     public  void debug(){
-        System.out.println("+ Step 1: Khởi chạy trình duyệt Chrome!...");
-        WebDriver driver;
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://demoqa.com/automation-practice-form");
-
-
-
-        WebElement submit = driver.findElement(By.xpath("//button[@id='submit']"));
-        submit.submit();
-
+        log.debug("debug log");
+        log.error("error log");
+        log.info("info log");
     }
 }
